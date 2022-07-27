f5d3fada137db33aa8f7c4d2e661982a5ad501e7
quanle <quan.leminh@hotmail.com>
2022-07-27 21:18:06 +0700

    updated package.

---
+++ iOS/HowtankWidgetSwift.xcframework/ios-arm64/HowtankWidgetSwift.framework/Headers/HowtankWidgetSwift.h
@@ -0,0 +1,19 @@
+//
+//  HowtankWidgetSwift.h
+//  HowtankWidgetSwift
+//
+//  Created by Damien Dorizy on 30/01/2018.
+//  Copyright © 2018 Howtank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HowtankWidgetSwift.
+FOUNDATION_EXPORT double HowtankWidgetSwiftVersionNumber;
+
+//! Project version string for HowtankWidgetSwift.
+FOUNDATION_EXPORT const unsigned char HowtankWidgetSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HowtankWidgetSwift/PublicHeader.h>
+
+
