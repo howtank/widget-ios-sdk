369a6666586ea83c51990204f880ea0448dae816
quanle <quan.leminh@hotmail.com>
2022-08-01 17:02:39 +0700

    updated package.

---
+++ cocoapod.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sources="https://github.com/howtank/widget-ios-sdk.git"
+
+# Grab pod name
+podspecName=$(basename $(find . -name *.podspec) | sed 's/.podspec//g' )
+version=$(awk '/\.version/' $podspecName.podspec | awk '/[0-9]\.[0-9]\.[0-9]/' | sed 's/.version//g'  | sed 's/[^0-9/.]//g')
+
+pod lib lint --allow-warnings --sources=$sources
+
+# Pod lint fail
+if [ $? != 0 ];then
+    exit 1
+fi
+
+git add -A
+git commit -m 'updated package.'
+git push
+
+git tag -m "update podspec" $version
+git push --tags
+
+pod repo push 'widget-ios-sdk' 'HowtankWidgetSwift.podspec' --allow-warnings
+
+git pull
--- make_build.sh
+++ make_build.sh
@@ -1,5 +1,35 @@
+version=$(node -p "require('./package.json').version") 
+echo "current version is ${version}."
+
+major=0
+minor=0
+build=0
+
+# break down the version number into it's components
+regex="([0-9]+).([0-9]+).([0-9]+)"
+if [[ $version =~ $regex ]]; then
+  major="${BASH_REMATCH[1]}"
+  minor="${BASH_REMATCH[2]}"
+  build="${BASH_REMATCH[3]}"
+  build=$(echo $build + 1 | bc)
+fi
+
+newVersion=${major}.${minor}.${build}
+
+sed -i '' "s/${version}/${newVersion}/" ./package.json
+sed -i '' "s/'${version}'/'${newVersion}'/" ./HowtankWidgetSwift.podspec
+cat ./package.json
+
+#remove old one
+rm -rf ./iOS/HowtankWidgetSwift.xcframework
+
 cd ./source
 git pull
 
 #run build framewok.
 sh ./build_xcframework.sh
+
+# updated podspec
+cd ..
+sh ./cocoapod.sh
+
