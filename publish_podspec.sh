dc46ef8cd57fa42fc540036632a7c8512cb636f9
quanle <quan.leminh@hotmail.com>
2022-08-02 16:19:50 +0700

    updated script

--- publish_podspec.sh
+++ publish_podspec.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-sources="https://github.com/howtank/widget-ios-sdk.git"
-
-# Grab pod name
+#Grab pod name
 podspecName=$(basename $(find . -name *.podspec) | sed 's/.podspec//g' )
 version=$(awk '/\.version/' $podspecName.podspec | awk '/[0-9]\.[0-9]\.[0-9]/' | sed 's/.version//g'  | sed 's/[^0-9/.]//g')
-
-pod lib lint --allow-warnings --sources=$sources
 
 # Pod lint fail
 if [ $? != 0 ];then
@@ -20,6 +16,8 @@ git push
 git tag -m "update podspec" $version
 git push --tags
 
-sha=$(git rev-parse HEAD)
+branchname=$(git rev-parse --abbrev-ref HEAD)
+sha=$(git rev-parse $branchname)
+ref=refs/heads/$version
 
-curl -X POST https://api.github.com/repos/howtank/widget-ios-sdk/git/refs -H "Accept: application/vnd.github+json" -H "Authorization: token $1" -d '{"ref":"refs/heads/'$version'", "sha":'$sha'}'
+curl -X POST https://api.github.com/repos/howtank/widget-ios-sdk/git/refs -H "Accept: application/vnd.github+json" -H "Authorization: token $1" -d '{"ref":"'$ref'", "sha":"'$sha'"}'
