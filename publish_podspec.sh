209c780583244e93032a4cd964013ac0f00922c3
quanle <quan.leminh@hotmail.com>
2022-08-03 09:54:09 +0700

    updated script

--- publish_podspec.sh
+++ publish_podspec.sh
@@ -6,16 +6,20 @@ version=$(awk '/\.version/' $podspecName.podspec | awk '/[0-9]\.[0-9]\.[0-9]/' |
 
 # Pod lint fail
 if [ $? != 0 ];then
+    echo 'Missing ACCESS_TOKEN. Please pass your PERSONAL_ACCESS_TOKEN as an argument of this command.'
     exit 1
 fi
 
+echo '============ Pushing the update ================'
 git add -A
 git commit -m 'updated package.'
 git push
 
+echo "============ Tag version $version ================"
 git tag -m "update podspec" $version
 git push --tags
 
+echo "============ create new branch for version $version ================"
 branchname=$(git rev-parse --abbrev-ref HEAD)
 sha=$(git rev-parse $branchname)
 ref=refs/heads/$version
--- update_version.sh
+++ update_version.sh
@@ -1,14 +1,18 @@
 sourceCodeVersion=$(node -p "require('./source/package.json').version") 
 sdkVersion=$(node -p "require('./package.json').version") 
+echo 'copy framework from source code repo.'
+  cp -Rv source/build/HowtankWidgetSwift.xcframework ./
 
 if [ "$sourceCodeVersion" == "$sdkVersion"  ]; then
   echo 'No new version detected.'
   exit 1
 else
+  echo '============ update version ================'
   sed -i '' "s/${sdkVersion}/${sourceCodeVersion}/" ./package.json
   sed -i '' "s/'${sdkVersion}'/'${sourceCodeVersion}'/" ./HowtankWidgetSwift.podspec
   sed -i '' "s/${sdkVersion}/${sourceCodeVersion}/" ./README.md
 
-  cp -R source/build/HowtankWidgetSwift.xcframework ./
+  echo '============ copy framework from source code repo ================'
+  cp -Rv source/build/HowtankWidgetSwift.xcframework ./
 fi
 
