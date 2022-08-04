9c53fdacb39dec5d159f8a6d0579fcb1c201d3c4
quanle <quan.leminh@hotmail.com>
2022-08-04 15:12:37 +0700

    added exclude folder/file

--- Package.swift
+++ Package.swift
@@ -2,6 +2,7 @@
 import PackageDescription
 let package = Package(
     name: "HowtankWidgetSwift",
+    exclude: ["source/", "package.json", "publish_podspec.sh", "update_version.sh"]
     platforms: [
         .iOS(.v12)
     ],
